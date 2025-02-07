31f92e8a333cf9fa084c494d15968290b31bd5d8
tusharXsuvera <developer1.suvera@gmail.com>
2025-02-07 16:16:45 +0530

    #footer design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,28 @@
+import { footerJson } from "../utils/JSON";
+
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="global_container top_container">
+      <div className="flex flex-row justify-evenly">
+        {footerJson.map((items, i) => {
+          const { heading, options } = items;
+          return (
+            <div key={i}>
+              <h1>{heading}</h1>
+              {options.map((items, i) => {
+                const { name } = items;
+                return (
+                  <div key={i}>
+                    <h3>{name}</h3>
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
